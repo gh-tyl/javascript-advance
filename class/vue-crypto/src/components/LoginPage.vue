7d968adbdcb5fda10db4b9717742a27ad3c4cf6f
Tyler Inari <github@al.tyler.jp>
2022-11-23 09:09:18 -0800

    done 2022-11-22

---
+++ class/vue-crypto/src/components/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username" />
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" />
+      <button type="submit">Login</button>
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log('login', this.username, this.password)
+    }
+  }
+}
+</script>
